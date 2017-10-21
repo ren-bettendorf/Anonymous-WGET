5d658d69b638df4828da00dd0fbcf6f792502d91
Ren Bettendorf <rbetten@phoenix.cs.colostate.edu>
2017-10-21 14:26:05 -0600

    Added a ton of functionality

--- ss.cpp
+++ ss.cpp
@@ -27,29 +27,84 @@ void cleanExit(int exitCode, string message)
 	exit(exitCode);
 }
 
-int handleNextStone(char* chainlistChar, char* url)
+void sendSystemWget(string url, string filename)
+{
+	cout << "wget " << url << endl;
+	const char* wgetMessage = ("wget " + url).c_str();
+	system(wgetMessage);
+}
+
+string createFinalRequestUrl(string url)
+{
+        cout << "Parsing: " << url << endl;
+	
+        if( contains(url, ("://")) )
+        {
+                cout << "String has beginning http(s). Stripping" << endl;
+                cout << "Found at position " << url.find("://") << endl;
+                url = url.substr(url.find("://") + 3);
+        }
+        int countSlashes = count(url.begin(), url.end(), '/');
+        if ( countSlashes < 2 )
+        {
+                if( countSlashes == 0 )
+                {
+                        cout << "Adding slash" << endl;
+                        url = url + "/";
+                }
+                url = url + "index.html";
+        }
+
+        cout << "FINAL: " << url << endl;
+	return url;
+}
+
+string parseFileName(string url)
+{
+	return url.substr(url.find_last_of("/") + 1);
+
+}
+
+vector<string> splitChainlistFromLastStone(char* chainlistChar)
 {
 	string chainlist = string(chainlistChar);
 
-	vector<string> splitChain;
+	vector<string> splitChainlist;
 	trim(chainlist);
-	split(splitChain, chainlist, is_any_of(" "), token_compress_on);
+	split(splitChainlist, chainlist, is_any_of(" "), token_compress_on);
 
+	return splitChainlist;
+}
+
+vector<string> removeCurrentStone(vector<string> chainlist, string currentStone)
+{
+	for(int i = 0; i < chainlist.size(); i++)
+	{
+		if( chainlist[i].compare(currentStone) )
+		{
+			chainlist.erase(chainlist.begin() + i);
+		}
+
+	}
+	return chainlist;
+}
+
+int selectRandomStoneIndex(int max)
+{
 	srand(time(NULL));
 
-	int randIndex = rand() % splitChain.size();
+	return rand() % max;
+}
 
-	string nextStone = splitChain.at(randIndex);
-	splitChain.erase(splitChain.begin() + randIndex);
 
-	vector<string> splitIpAndPort;
-	split(splitIpAndPort, nextStone, is_any_of(":"));
 
+int connectToNextStone(string ip, int port)
+{
 	struct sockaddr_in nextStoneAddr;
 
 	nextStoneAddr.sin_family = AF_INET;
-        nextStoneAddr.sin_addr.s_addr = inet_addr(splitIpAndPort.at(0).c_str());
-       	nextStoneAddr.sin_port = htons(stoi(splitIpAndPort.at(1)));
+        nextStoneAddr.sin_addr.s_addr = inet_addr(ip.c_str());
+       	nextStoneAddr.sin_port = htons(port);
 	int nextStoneSock;
 
         if ( (nextStoneSock = socket(AF_INET, SOCK_STREAM, 0)) < 0 )
@@ -66,31 +121,6 @@ int handleNextStone(char* chainlistChar, char* url)
 	return nextStoneSock;
 }
 
-void grabFile(char* url)
-{
-
-}
-
-void sendFileToPreviousStone(int sock)
-{
-
-}
-
-void waitForFile(int sock)
-{
-
-}
-
-void deserializePacket() 
-{
-    
-}
-
-void deserializeInitialPacket(char* message)
-{
-	
-}
-
 int displayHelpInfo() 
 {
 	cout << "Welcome to Stepping Stone!" << endl << endl;
@@ -100,7 +130,7 @@ int displayHelpInfo()
 }
 
 
-void handleConnectionThread(int port, int previousStoneSock) 
+void handleConnectionThread(string ip, int port, int previousStoneSock) 
 {
 	int nextStoneSock;
 	char messageHeaderBuffer[4];
@@ -130,16 +160,17 @@ void handleConnectionThread(int port, int previousStoneSock)
 	char url[sizeUrl];
 	memcpy(url, messageBuffer + sizeChainlist, sizeUrl);
 
-	if(sizeChainlist > 0)
+	vector<string> splitChainlist = splitChainlistFromLastStone( chainlist );
+	splitChainlist =  removeCurrentStone(splitChainlist, ip + ":" + to_string(port) );
+
+
+	if(splitChainlist.size() > 0)
 	{
-		nextStoneSock = handleNextStone(chainlist, url);
-		waitForFile(nextStoneSock);
-		
+
 	}
 	else
 	{
-		grabFile(url);
-		sendFileToPreviousStone(previousStoneSock);
+
 	}
 }
 
@@ -211,8 +242,9 @@ int main(int argc, char* argv[])
 			string errorMessage("Error: Problem accepting client.");
 			cleanExit(1, errorMessage);
 		}
+		string ipString(ip);
 
-		thread ssSockThread(handleConnectionThread, port, incomingSock);
+		thread ssSockThread(handleConnectionThread, ip, port, incomingSock);
 		ssSockThread.join();
 	}
     
